b59ebc38b6e0ae392dbfcaa4a8be7bed33ef3e4f
Biaginibe <bia.araujo.pinto@gmail.com>
2025-02-17 10:38:00 -0300

    feat: update deployment workflow and configure build output directory

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   base: "/rock-paper-scissors-lizard-Spock/", // Corrige o caminho no GitHub Pages
+  build: {
+    outDir: "dist", // O build será gerado na pasta dist (padrão do Vite)
+  },
 });
